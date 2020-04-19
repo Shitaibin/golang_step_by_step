9b859b36b33a40ef439ad0d39c9e7113ed8bc113
Shitaibin <hz_stb@163.com>
2020-04-19 20:16:21 +0800

    channel
    Signed-off-by: Shitaibin <hz_stb@163.com>

--- channel/nil/nil.go
+++ channel/nil/nil.go
@@ -6,13 +6,16 @@ import (
 )
 
 func main() {
+	// default value of ch is nil
 	var ch chan int
 	go func() {
+		// ch point to an new channel object
 		ch = make(chan int, 1)
 		ch <- 1
 		fmt.Println("g1 exit")
 	}()
 
+	// nil channel pass to function
 	go func(ch chan int) {
 		if ch == nil {
 			fmt.Println("ch is nil")
